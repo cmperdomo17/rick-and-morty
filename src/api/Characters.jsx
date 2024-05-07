4b849f72c2cb8842e8ae70aec0061f5f47473138
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-05-06 21:20:36 -0500

    Update UI styles and layout in App and Characters components

--- src/api/Characters.jsx
+++ src/api/Characters.jsx
@@ -23,13 +23,13 @@ function Characters() {
 
     return (
         <div className='flex flex-col my-6 mx-6 gap-6'>
-            <div className='flex flex-row justify-between text-center'>
+            <div className='flex flex-row justify-between text-center gap-6 md:gap-0'>
                 <Link to='/'>
-                    <button className='bg-orange-200 hover:bg-orange-300 text-stone-800 font-bold py-2 px-4 rounded-full'>
+                    <button className='bg-orange-200 hover:bg-orange-300 text-stone-800 font-bold py-1 px-2 md:py-2 md:px-4 rounded-full'>
                         <svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-back-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 14l-4 -4l4 -4" /><path d="M5 10h11a4 4 0 1 1 0 8h-1" /></svg>
                     </button>
                 </Link>
-                <h1 className='text-4xl font-extrabold text-orange-200 font-sans pr-16'>CHARACTERS</h1>
+                <h1 className='text-4xl font-extrabold text-orange-200 pr-16'>CHARACTERS</h1>
                 <div></div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -45,6 +45,7 @@ function Characters() {
                 ))}
             </div>
         </div>
+        
     )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Card ({id, name, status, species, type, gender, origin, location, image}) {
     return (
-        <div className=" bg-zinc-800 p-4 rounded-3xl shadow-md font-sans">
+        <div className=" bg-zinc-800 p-4 rounded-3xl shadow-md">
             <div className="flex justify-center flex-col items-center gap-2">
                 <img src={image} alt={name} className="w-1/2 object-cover mb-4 mt-2 rounded-full shadow-2xl shadow-black hover:scale-110 transition duration-300 ease-in-out" />
                 <h2 className="text-4xl font-extrabold text-orange-300 text-center">{name}</h2>
